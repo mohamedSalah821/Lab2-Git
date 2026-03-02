a6e13d8f99b2871495f441f6c3635c930124cf9d
mohamed salah <mohamedsalahsabry88@gmail.com>
2026-03-02 11:18:01 +0200

    Add HelloGit() function

--- HelloITI.java
+++ HelloITI.java
@@ -4,4 +4,8 @@ public static void printHelloITI() {
         System.out.println("Hello ITI");
     }
 
+     public static void printHelloGit() {
+        System.out.println("Hello Git");
+    }
+
 }
