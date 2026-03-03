3af4a4e7da6eb950e4400727ece7d9c4e2fb63d3
mohamed salah <mohamedsalahsabry88@gmail.com>
2026-03-03 10:55:11 +0200

    Add First Commit mohamed

--- HelloJava.java
+++ HelloJava.java
@@ -2,6 +2,8 @@ public class HelloJava  {
 
  public static void printHelloWorld() {
         System.out.println("Hello World");
+        System.out.println("Hello From Mohamed salah");
+
     }
 
 }
